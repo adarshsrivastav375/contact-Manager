857d717062d331c1490957148c87f8ac037b4427
Adarsh srivastav <adarshsrivastav375@gmail.com>
2023-05-15 14:30:45 +0530

    added previous routes with basename

--- src/App.js
+++ src/App.js
@@ -1,27 +1,66 @@
 import './App.css';
 import AddContacts from './components/AddContacts';
 import UpdateContacts from './components/UpdateContact';
-import { Routes, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 import ContactList from './components/ContactList';
 import Header from './components/Header'
  
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<ContactList/>,
+  },
+  {
+    path: "/add",
+    element: <AddContacts/>,
+  },
+  {
+    path:'/:id/update',
+    element:<UpdateContacts/>
+  }
+  
+], {basename: '/contacts'});
+
 function App() {
+  // const LOCAL_STORAGE_KEY ="contacts";
+  // const [contacts,setContacts]=useState([]);
+  // const addContactHandler = (contact)=>{
+  //   setContacts([...contacts,{id : uuidv4(), ...contact}]);
+  // };
+
+  // const removeContactHandler = (id) =>{
+  //   const newContactList =contacts.filter((contact)=>{
+  //     return contact.id !== id;
+  //   });
+  //   setContacts(newContactList);
+  // }
+
+  // useEffect(()=>{
+  //  let retrive = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  //   if (retrive && retrive.length) { setContacts(retrive);
+  //     console.log(retrive);
+  //   }
+  // },[]);
+
+  // useEffect(()=>{
+  //   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  // },[contacts]);
+  
+
   return (
     <div className="ui container">
-      <Header />
-      <Routes>
-        <Route path="/" element={<ContactList />} />
-        <Route path="/add" element={<AddContacts />} />
-        <Route path="/:id/update" element={<UpdateContacts />} />
-      </Routes>
+      {/* <Header/>
+      <AddContacts addContactHandler={addContactHandler}/>
+      <ContactList contacts={contacts} getContactId={removeContactHandler}/> */}
+       <Header/>
+       <RouterProvider router={router} />
+
     </div>
   );
 }
-
-
-
-
-
 
 export default App;
